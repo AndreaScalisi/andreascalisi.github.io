6e21d5710831928b1bc4e176a318204f93131a52
Andrea <andrea.scalisi@epfl.ch>
2019-12-12 15:02:31 +0100

    Add about section and fix list centered issue

--- maps/map.js
+++ maps/map.js
@@ -28,7 +28,7 @@ const paris_coord = [48.860716, 2.329014];
 //Function to draw the map with the clusters
 async function drawMap(year, stored = true){
 	//Create the map
-	const mymap = L.map('mapid', {minZoom: 12}).setView(L.latLng(paris_coord), 12);
+	const mymap = L.map('mapid', {minZoom: 12}).setView(L.latLng(paris_coord), 13);
 
 	var mapboxUrl = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw";
 	var mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
